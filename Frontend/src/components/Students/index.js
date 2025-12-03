5a71a80241aff64f81342eec7b40c6d8fa29593a
Lello <lello.coelho@gmail.com>
2025-12-03 11:26:51 -0300

    Alterado o estilo do messagetoast usando o padrao do login.

--- Frontend/src/components/Students/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Students } from "./Students";
-export { default as StudentForm } from "./StudentForm";
-export { default as StudentSearch } from "./StudentSearch";
--- Frontend/src/components/miscellaneous/MessageToast.jsx
+++ Frontend/src/components/miscellaneous/MessageToast.jsx
@@ -3,14 +3,16 @@ import PropTypes from "prop-types";
 function MessageToast({ messageToast }) {
   if (!messageToast || !messageToast.text) return null;
 
-  let bgColor = "bg-yellow-600";
-  if (messageToast.type === "success") bgColor = "bg-green-600";
-  else if (messageToast.type === "error") bgColor = "bg-red-600";
+  let bgColor = "bg-yellow-100 text-yellow-700 border-yellow-500";
+  if (messageToast.type === "success")
+    bgColor = "bg-green-100 text-green-700 border-green-500";
+  else if (messageToast.type === "error")
+    bgColor = "bg-red-100 text-red-700 border-red-500";
 
   return (
     <div
-      className={`fixed top-4 right-4 z-50 px-4 py-2 rounded text-white shadow-lg ${bgColor}`}
-      style={{ minWidth: 200, textAlign: "center" }}
+      className={`fixed bottom-4 right-4 z-50 p-4 rounded-md shadow-lg max-w-md w-auto border-2 ${bgColor}`}
+      //style={{ minWidth: 200, textAlign: "center" }}
     >
       {messageToast.text}
     </div>
